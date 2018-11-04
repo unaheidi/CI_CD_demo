59c7bcc661f7079ddffb5d71d6f3ab9ce0cc1e91
suling <su_l@ctrip.com>
2018-11-04 19:20:38 +0000

    Update updateAndRestart.sh

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -23,9 +23,9 @@ npm remove pm2 -g
 
 #pm2 needs to be installed globally as we would be deleting the repo folder.
 # this needs to be done only once as a setup script.
-npm install pm2 -g
+#npm install pm2 -g
 # starting pm2 daemon
-pm2 status
+#pm2 status
 
 cd /home/ec2-user/ci_cd_demo
 
